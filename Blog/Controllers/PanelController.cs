3023afacaea20e12181081d92da4250c44434630
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-02-25 12:11:10 -0500

    Added Admin Panel functions for blog posts

---
+++ Blog/Controllers/PanelController.cs
@@ -0,0 +1,72 @@
+﻿using Blog.Data.Repository;
+using Blog.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PanelController : Controller
+    {
+        private IRepository _repo;
+
+        public PanelController(IRepository repo)
+        {
+            _repo = repo;
+        }
+        
+        public IActionResult Index()
+        {
+            // We are getting all the posts here
+            var posts = _repo.GetAllPosts();
+            return View(posts);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Post());
+            }
+            else
+            {
+                var post = _repo.GetPost((int)id);
+                return View(post);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            if (post.Id > 0)
+            {
+                _repo.UpdatePost(post);
+            }
+            else
+            {
+                _repo.AddPost(post);
+            }
+
+            if (await _repo.SaveChangesAsync())
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(post);
+            }
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Remove(int id)
+        {
+            _repo.RemovePost(id);
+            await _repo.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -25,15 +25,20 @@ namespace Blog
         {
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_config["DefaultConnection"]));
 
-            services.AddDefaultIdentity<IdentityUser>(options => {
+            services.AddIdentity<IdentityUser, IdentityRole>(options => {
                 options.Password.RequireDigit = false;
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequireUppercase = false;
                 options.Password.RequiredLength = 6;
 
             })
-                .AddRoles<IdentityRole>()
+                //.AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+            });
           
             services.AddTransient<IRepository, Repository>();
             
@@ -51,6 +56,8 @@ namespace Blog
             }
 
             app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseRouting();
 
